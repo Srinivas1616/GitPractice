3b058ca26a1d8631e3bc6e4b3d4a905fff113827
Srinivas Natikar <Srinivas.Natikar@msg-global.com>
2020-12-19 11:06:45 +0530

    commit check

--- src/com/practice/arrayDemo/DemoStage1.java
+++ src/com/practice/arrayDemo/DemoStage1.java
@@ -3,6 +3,6 @@ package com.practice.arrayDemo;
 public class DemoStage1 {
 
 	public static void main(String[] args) {
-		System.out.println("hellooooo");
+		System.out.println("hellooooo sinu");
 	}
 }
