85492748d9e8b0044a72b0b6d4c97ddbaa124a7a
Srinivas Natikar <Srinivas.Natikar@msg-global.com>
2020-12-20 18:53:43 +0530

    linkedlist added

--- src/com/practice/arrayDemo/DemoStage1.java
+++ src/com/practice/arrayDemo/DemoStage1.java
@@ -7,7 +7,17 @@ public class DemoStage1 {
 
 	public static void main(String[] args) {
 		System.out.println("hellooooo sinu");
+		LinkedList<String> ls = new LinkedList<>();
+		ls.add("yello");
+		ls.add("red");
+		ls.add("orange");
+		ls.add("blue");
+		ls.add("green");
 		
+		Iterator<String> it = ls.iterator();
+		while(it.hasNext())
+			ls.remove();
+		System.out.println(ls);
 
 	}
 }
