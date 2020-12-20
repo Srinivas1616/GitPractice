22b36b89dba2b135598e92d77eacab0c9127a8e2
Srinivas Natikar <Srinivas.Natikar@msg-global.com>
2020-12-20 18:52:06 +0530

    just commit

--- src/com/practice/arrayDemo/DemoStage1.java
+++ src/com/practice/arrayDemo/DemoStage1.java
@@ -1,13 +1,13 @@
 package com.practice.arrayDemo;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class DemoStage1 {
 
 	public static void main(String[] args) {
 		System.out.println("hellooooo sinu");
-<<<<<<< HEAD
 		
-		System.out.println("hellooooo this devSinu branch");
-=======
->>>>>>> 6d16be5fd2343e8ca5d3b10a7e339ae25c34ab70
+
 	}
 }
